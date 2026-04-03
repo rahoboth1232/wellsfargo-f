0cb20dbeeef87d47f232fd3ed328519ce560c909
rahoboth1232 <rahoboth05@gmail.com>
2026-04-03 14:14:55 +0530

    change in api

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -38,7 +38,7 @@ api.interceptors.response.use(
       try {
 
         const res = await axios.post(
-          "http://127.0.0.1:8000/api/token/refresh/",
+          "/api/token/refresh/",
           {
             refresh: refreshToken
           }
